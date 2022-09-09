b33729ddeacc6623c09cfdcd59dfc1a940771bf5
paul7299 <paul7299@stud.kea.dk>
2022-09-09 10:28:10 +0200

    Added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public static void main(String[] args) {
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+        sg.greet();
+
     }
 
     public void start() {
         System.out.println("Det virker!");
-
     }
+
+    public void greet() {
+        System.out.println("- Indtast dit navn:");
+        Scanner sc = new Scanner(System.in);
+        String navn = sc.nextLine();
+        System.out.println("Hej " + navn);
+    }
+
+
 }
