1004cc7e0ce4e6e78da76701f9f1fd53f903aa25
paul7299 <paul7299@stud.kea.dk>
2022-09-09 09:57:28 +0200

    First commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,11 @@
+public class SuperGreeter {
+    public static void main(String[] args) {
+        SuperGreeter sg = new SuperGreeter();
+        sg.start();
+    }
+
+    public void start() {
+        System.out.println("Det virker!");
+
+    }
+}
